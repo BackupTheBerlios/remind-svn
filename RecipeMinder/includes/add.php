d33f2ec274c33d895b30562731ede1bcf1aab1f6
huey <huey@634eee4f-3a0c-0410-bd97-9f04d24c3c66>
2006-02-14 01:24:24 +0000

    Improved display of already entered ingredients as well as indicating searchable ones and added link to perform search on clicked ingredient.

--- RecipeMinder/includes/add.php
+++ RecipeMinder/includes/add.php
@@ -41,88 +41,6 @@ echo '<form name="Addition" action="add.php" method="get">';
 // first time through, ask for name
 switch ( $_GET['button'])
 {
-	case "Recipe":
-		require($baseUrl."/includes/add_recipe.php");
-/*		echo 'Name: <input type="text" name="rname" size="80" value="'.$_GET['rname'].'"><br><br>';
-		echo 'Type:  <select name="rtype" size="1" id="rtype" class="text">
-				<option'.(($_GET['rtype']=='Appetizer')?'selected="selected"':'').'>Appetizer</option>
-				<option'.(($_GET['rtype']=='Dessert')?'selected="selected"':'').'>Dessert</option>
-				<option'.(($_GET['rtype']=='Drink')?'selected="selected"':'').'>Drink</option>
-				<option'.(($_GET['rtype']=='Entree')?'selected="selected"':'').'>Entree</option>
-				<option'.(($_GET['rtype']=='Snack')?'selected="selected"':'').'>Snack</option>
-				<option'.(($_GET['rtype']=='Other')?'selected="selected"':'').'>Other</option>
-				</select>&#09;';
-		echo 'Portions: <input type="text" name="rportions" size="2" value="'.(int)$_GET['rportions'].'">&#09;';
-		echo 'Number of Ingredients:  <input type="text" size="2" name="ing_count" value="'.$_GET['ing_count'].'"><br><br>';
-		echo '<input type="submit" name="button" value="Next">';
-		break;
-	case "Next":
-		// insert into database
-		$ins='insert into main (name, instructions, portions, type) values ("';
-		$ins.=$_GET['rname'].'", "x", ';
-		$ins.=$_GET['rportions'].', "';
-		$ins.=$_GET['rtype'].'")';
-//		echo "$ins<br>";
-		if ( mysql_query($ins) == FALSE )
-		{
-			echo "Error: (".mysql_errno($link) .") - ".mysql_error($link); 
-		}
-		else
-		{
-			$rindex=mysql_insert_id();
-			$inglist="";
-			$query='select ing_name, ing_index from ingredients order by ing_name asc';
-			$result=mysql_query($query) or die('Query failed: ' . mysql_error());
-			while ($ings=mysql_fetch_assoc($result))
-			{
-				$inglist.='<option value="'.$ings['ing_index'].'"> '.$ings['ing_name'].'</option>';
-			}
-			echo '<input type="hidden" name="indexval" value="'.$rindex.'">';
-			echo '<h3>'.$_GET['rname'].'</h3>'.$_GET['rtype'].' for '.$_GET['rportions'].'<br>';
-			echo '<table width="60%"><tr><th width="15%">Amount</td><th width="20%">Measure</td><th>Ingredient</td></tr>'."\n";
-			for ($rnum=0; $rnum<$_GET['ing_count']+0;$rnum++)
-			{
-					  echo '<tr><td><input type="text" name="amt'.$rnum.'"></td>';
-				echo '<td><input type="text" name="measure'.$rnum.'"></td>';
-				echo '<td><select name="ing'.$rnum.'">'.$inglist.'</option></td></tr>'."\n";
-			}
-			echo '</table>'."\n";
-			echo '<textarea id="instructions" cols="80" rows="15" class="textarea" name="instructions"></textarea><br>'."\n";
-			echo 'Key Words:&nbsp;&nbsp;<input type="text" name="keywords" size="80"><br>';
-			echo '<input type="submit" name="button" value="Cancel">&#09;&#09;&#09;&#09;&#09;<input type="submit" name="button" value="Save">';
-			echo '</form>';
-		}
-		break;
-	case "Cancel":
-		$del='delete from main where rindex = '.$_GET['indexval'];
-		mysql_query($del);
-		echo "Recipe creation canceled . . .";
-		break;
-	case "Save":
-		$recipe_index=$_GET['indexval'];
-		for($i=0;;$i++)
-		{
-			if ( $_GET["amt{$i}"] == "" )
-			{
-				break;
-			}
-//			echo $i.')  amt: '.$_GET["amt$i"].' measure:  '.$_GET["measure$i"].' ing_index:  '.$_GET["ing$i"].'&nbsp;&nbsp;';
-			$insamt=toDecimal($_GET["amt$i"]);
-			$query='insert into links values (';
-			$query.=$recipe_index;
-			$query.=', '.$_GET["ing$i"].', '.$insamt.', "'.$_GET["measure$i"].'")';
-//			echo $query.'<br>';
-			mysql_query($query);
-		}	
-		$query='update main set instructions="'.$_GET['instructions'].'" where rindex='.$recipe_index;
-		echo $query.'<br>';
-		mysql_query($query);
-		if (strlen($_GET['keywords']))
-		{
-			$query='insert into keywords values('.$recipe_index.', "'.$_GET['keywords'].'")';
-			mysql_query($query);
-		} */
-		break;
 	case "Add Ingredient":
 		$newIngredient=stripcslashes($_GET['ingredient']);
 		if (strlen($newIngredient))
@@ -155,23 +73,69 @@ switch ( $_GET['button'])
 			}
 		}
 	case "Ingredient":
-		echo "<font color=\"#00008C\">";
-		$ingNum=1;
-		$query="select ing_name from ingredients order by ing_name asc";
-		$ingList=mysql_query($query);
-		while ($nextIng=mysql_fetch_assoc($ingList))
-		{
-			if ($ingNum % 2)
-				echo "<font color=\"#8C223D\">";
-			echo $nextIng['ing_name']." ";
-			if ($ingNum % 2)
-				echo "</font>";
-			$ingNum++;
-		}
-		echo '<hr size="5" width="100%"/>';
 		echo '</font>Ingredient: <input type="text" name="ingredient" size="40"><br>';
 		echo '<input type="checkbox" name="add_search" value="TRUE"> Add to searchlist<br><br>';
 		echo '<input type="submit" name="button" value="Add Ingredient">';
+		echo '<hr size="5" width="100%"/>';
+		echo '<font color="#00008C">';
+		$query="select distinct upper(substring(ing_name,1,1)) from ingredients";
+		$letters=mysql_query($query);
+		$numletters=mysql_num_rows($letters);
+		$query="select * from ingredients order by ing_name asc";
+		$ingList=mysql_query($query);
+		$numrows=mysql_num_rows($ingList)+$numletters;
+		$longCols=$numrows % 4;
+		$rowspercol=($numrows - $longCols)/4;
+		if ($longCols)
+			$rowspercol++;
+		echo '<table width="100%"><tr><td align="center" valign="top" width="25%"><font color=\"#00008C\">';
+		$lastLetter="";
+		$rowcount=0;
+		$letterCount=1;
+		while ($nextIng=mysql_fetch_assoc($ingList))
+		{
+			if ($lastLetter != strtoupper(substr($nextIng['ing_name'],0,1)))
+			{
+				echo "<h5 style=\"color:#FFFFFF; background:#8C8C00\">";
+				$lastLetter=strtoupper(substr($nextIng['ing_name'],0,1));
+				echo $lastLetter.'</h5>';
+				$rowcount++;
+				if (($rowcount % $rowspercol) == 0)
+				{
+					echo '</td><td align="center" valign="top" width="25%"><font color=\"#00008C\">';
+					if ($longCols > 0)
+					{
+						$longCols--;
+						if ($longCols == 0)
+						{
+							$rowspercol--;
+							$rowcount--;
+						}
+					}
+				}
+			}
+			if ( strtoupper($nextIng['searchable']) == 'YES' )
+				echo '<a href="search.php?stype=Ingredients&smatch='.$nextIng['ing_name'].'">';
+			echo $nextIng['ing_name'];
+			if ( strtoupper($nextIng['searchable']) == 'YES' )
+				echo ' *</a>';
+			echo '<br>';
+			$rowcount++;
+			if (($rowcount % $rowspercol) == 0)
+			{
+				echo '</td><td align="center" valign="top"><font color=\"#00008C\">';
+				if ($longCols > 0)
+				{
+					$longCols--;
+					if ($longCols == 0)
+					{
+						$rowspercol--;
+						$rowcount--;
+					}
+				}
+			}
+		}
+		echo '</td></tr></table>';
 		break;
 }
 ?>
